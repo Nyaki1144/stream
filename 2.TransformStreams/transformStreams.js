9edde45dd4223cc7b53dac89222e689ff61afd4f
nyaki <narek1144@gmail.com>
2024-08-06 10:21:57 +0400

    fix: fix 2 task

--- 2.TransformStreams/transformStreams.js
+++ 2.TransformStreams/transformStreams.js
@@ -3,12 +3,14 @@ import __dirname from "../path.js";
 import { Transform } from "stream";
 import fs from "fs";
 
-const data = fs.createReadStream(path.join(__dirname, "2.TransformStreams", "text.txt"));
-const transformFile = fs.createWriteStream(path.join(__dirname, "2.TransformStreams", "transform.txt"));
+const data = fs.createReadStream(path.join(__dirname, "2.TransformStreams", "text.json"));
+const transformFile = fs.createWriteStream(path.join(__dirname, "2.TransformStreams", "transform.json"));
 
 const transform = new Transform({
   transform(chunk, encoding, callback) {
-    this.push(chunk.toString().toUpperCase());
+    const part = chunk.toString();
+    this.push(part.replaceAll("}", `,\n"timestamp": "${new Date()}"}`));
+    callback();
   },
 });
 
