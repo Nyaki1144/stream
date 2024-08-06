140558a1933d4c45c08e467cd4ed72cf4439ac44
nyaki <narek1144@gmail.com>
2024-08-06 11:20:14 +0400

    5. Real-time Data Processing(Optional)

--- 5.Real-timeDataProcessing(Optional)/index.js
+++
@@ -1,7 +0,0 @@
-const input = document.querySelector("#input");
-const btn = document.querySelector("#btn");
-
-btn.addEventListener("click", (event) => {
-  event.preventDefault();
-  console.log(input.value);
-});
